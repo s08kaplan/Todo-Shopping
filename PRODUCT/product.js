f941445896305bb48c34c0af8669287e2b09ab95
Serkan <s8ersis@gmail.com>
2024-02-25 13:36:00 +0300

    shopping search added

--- PRODUCT/product.js
+++ PRODUCT/product.js
@@ -1,31 +1,58 @@
-
-const urlParams = new URLSearchParams(window.location.search)
-const productId = urlParams.get('id')
-const productShow = document.querySelector(".chosen-product")
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get("id");
+const productShow = document.querySelector(".chosen-product");
 
 const getProduct = async (productId) => {
-    try {
-     const res = await fetch(`https://fakestoreapi.com/products/${productId}`)
-    const data = await res.json()
-    console.log(data);  
-    return showItem(data)  
-    } catch (error) {
-        console.log("oops there is a problem", error);
-    }
-   
-}
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    const data = await res.json();
+    console.log(data);
+    return showItem(data);
+  } catch (error) {
+    console.log("oops there is a problem", error);
+  }
+};
 
 const showItem = (item) => {
-    console.log(item);
-    productShow.innerHTML = `
-        <div class="card">
+  console.log(item);
+  productShow.innerHTML = `
+    <div class="card">
+        <div class="card-info">
             <h3>${item.title}</h3> 
             <img src=${item.image} alt="product" width="400px">
+            <div>${item.description}</div>
             <div>${item.price} $</div>
         </div>
+        <div class="buttons">
+          <button>-</button>
+          <span>1</span>
+          <button>+</button>
+        </div>
+        <span class="total">Total:${item.price} $</span>
+    </div>
     `;
-}
+    const quantity = document.querySelector("span")
+  let quantityOfItem = document.querySelector("span").textContent;
+  let total = document.querySelector(".total");
+  const buttons = document.querySelector(".buttons");
+  buttons.addEventListener("click", (e) => {
+    if (e.target.textContent.includes("-")) {
+      if (quantityOfItem < 1) {
+        return (quantityOfItem = 1 && item.price * quantityOfItem);
+      } else if (quantityOfItem > 1) {
+        quantityOfItem--;
+        quantity.innerText = quantityOfItem
+        total.textContent = `Total: ${item.price * quantityOfItem} $`;
+        return total.textContent;
+      }
+    }
+    if (e.target.textContent.includes("+")) {
+      quantityOfItem++;
+      quantity.innerText =quantityOfItem
+      total.textContent = `Total: ${item.price * quantityOfItem} $`;
+      return total.textContent;
+    }
+  });
+};
 
-getProduct(productId)
-
-
+getProduct(productId);
--- SHOPPING/shopping.js
+++ SHOPPING/shopping.js
@@ -1,38 +1,62 @@
+const cardSection = document.querySelector(".cards-main");
+const searchInput = document.querySelector("#products");
+const searchButton = document.querySelector("button");
 
-const cardSection = document.querySelector(".cards-main")
-
-const getProducts = async (endpoint="products" ) => {
-    try {
-       const res = await fetch(`https://fakestoreapi.com/${endpoint}`)
-       const data = await res.json();
-       console.log(data);
-       return showProducts(data) 
-    } catch (error) {
-        console.log("Products couldn't fetch",error);
-    }
-}
+const getProducts = async (endpoint = "products") => {
+  try {
+    const res = await fetch(`https://fakestoreapi.com/${endpoint}`);
+    const data = await res.json();
+    console.log(data);
+    showProducts(data);
+    return data;
+  } catch (error) {
+    console.log("Products couldn't fetch", error);
+  }
+};
 
 const showProducts = (products) => {
-    console.log(products);
-    products.forEach(product => {
-        const productCard = document.createElement("div")
-        productCard.classList.add("card-container")
+  console.log(products);
+  cardSection.innerHTML = "";
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.classList.add("card-container");
 
-        productCard.innerHTML += `
+    productCard.innerHTML += `
         <div class="card">
            <h3>${product.title}</h3> 
            <img src=${product.image} alt="product" width="400px">
            <div>${product.price} $</div>
         </div>   
-        `
-        cardSection?.appendChild(productCard)
-        productCard.addEventListener("click",() => {
-            console.log(window.location.pathname)
-            window.location.href = `../PRODUCT/productPage.html?id=${product.id}`
-        }) 
+        `;
+    cardSection?.appendChild(productCard);
+    productCard.addEventListener("click", () => {
+      console.log(window.location.pathname);
+      window.location.href = `../PRODUCT/productPage.html?id=${product.id}`;
+    });
+  });
+};
 
-    })  
-}
+getProducts();
 
-getProducts()
+const getSearchedProducts = (data, inputValue) => {
+  return data.filter((info) =>
+    info.title.toLowerCase().includes(inputValue.toLowerCase())
+  );
+};
 
+const handleSearch = async () => {
+  const inputValue = searchInput.value;
+  if (!inputValue) {
+    getProducts();
+  } else {
+    const allProducts = await getProducts();
+    const searchedProducts = getSearchedProducts(allProducts, inputValue);
+    showProducts(searchedProducts);
+  }
+};
+
+searchInput.addEventListener("keyup", (e) => {
+  e.key === "Enter" && handleSearch();
+});
+
+searchButton.addEventListener("click", handleSearch);
